d57222be90e13835b98e5a571fa38159d99daa3d
whg <123456>
2020-10-21 23:00:35 +0800

    把本地仓库

---
+++ git-test-project/src/com/itcast/git/FirstClass.java
@@ -0,0 +1,24 @@
+package com.itcast.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+
+
+    }
+
+    public void init(){
+        int i=0;
+    }
+}
