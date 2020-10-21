53e41a565403f59da21c0302ad8fb5312e12a2f8
whg <123456>
2020-10-21 23:01:25 +0800

    把本地仓库修改

--- git-test-project/src/com/itcast/git/FirstClass.java
+++ git-test-project/src/com/itcast/git/FirstClass.java
@@ -14,11 +14,10 @@ public class FirstClass {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
-
-
     }
 
     public void init(){
         int i=0;
+        i += 10;
     }
 }
